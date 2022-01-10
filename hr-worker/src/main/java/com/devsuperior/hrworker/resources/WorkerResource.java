17c585c84403c89a2ff09e95621b729cd92c6864
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-01-10 10:46:46 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -1,11 +1,11 @@
 package com.devsuperior.hrworker.resources;
 
-import com.devsuperior.hrworker.entities.Worker;
-import com.devsuperior.hrworker.repositories.WorkerRepository;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.devsuperior.hrworker.entities.Worker;
+import com.devsuperior.hrworker.repositories.WorkerRepository;
 
 
 @RestController
@@ -22,6 +23,9 @@ public class WorkerResource {
 
     //This is for testing purposes
     private static Logger logger =  LoggerFactory.getLogger(WorkerResource.class);
+    
+    @Value("${test.config}")
+    private String testConfig;
 
     //This is for testing purposes
     @Autowired
@@ -29,6 +33,13 @@ public class WorkerResource {
 
     @Autowired
     private WorkerRepository repository;
+    
+    @GetMapping(value="/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("CONFIG = "+testConfig);
+        return ResponseEntity.noContent().build();
+    }
+    
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
